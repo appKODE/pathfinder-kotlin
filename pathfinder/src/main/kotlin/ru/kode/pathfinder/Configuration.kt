4ae9db3fdd6c9e18ba1547f6dcaa90f20ac7e5fb
KODE LLC <vs@kode.ru>
2023-05-10 13:32:00 +0200

    Add automatic change detection
    Stop using manual versioning, it is not very reliable and not very easy
    for the user.

--- pathfinder/src/main/kotlin/ru/kode/pathfinder/Configuration.kt
+++ pathfinder/src/main/kotlin/ru/kode/pathfinder/Configuration.kt
@@ -1,8 +1,45 @@
 package ru.kode.pathfinder
 
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 data class Configuration(
-  val version: Int,
   val environments: List<Environment>,
   val urlSpecs: List<UrlSpec>,
   val defaultEnvironmentId: EnvironmentId,
-)
+) {
+  fun computeChecksum(): String {
+    val envKey = buildString {
+      append(environments.size)
+      environments.forEach { e ->
+        append(e.id)
+        append(e.name)
+        append(e.queryParameters?.joinToString().orEmpty())
+        append(e.baseUrl)
+      }
+    }
+    val urlsKey = buildString {
+      append(urlSpecs.size)
+      urlSpecs.forEach { spec ->
+        append(spec.id)
+        append(spec.httpMethod.name)
+        append(spec.pathTemplate)
+        append(spec.name)
+      }
+    }
+    return try {
+      val digest = MessageDigest.getInstance("MD5")
+      digest.update(envKey.toByteArray())
+      digest.update(urlsKey.toByteArray())
+      digest.update(defaultEnvironmentId.value.toByteArray())
+      digest.digest().toHexString()
+    } catch (e: NoSuchAlgorithmException) {
+      println("md5 algorithm not found, falling back to plain strings")
+      envKey + urlsKey
+    }
+  }
+}
+
+private fun ByteArray.toHexString(): String {
+  return joinToString("") { (0xFF and it.toInt()).toString(16).padStart(2, '0') }
+}
